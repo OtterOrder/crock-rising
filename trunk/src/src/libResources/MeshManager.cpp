2994921aae0601f57353901e692aa4fab3579b3b
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-05 16:10:21 +0000

    Squelette des classes de ResourceManager.

--- trunk/src/src/libResources/MeshManager.cpp
+++ trunk/src/src/libResources/MeshManager.cpp
@@ -1,3 +1,19 @@
 #include "MeshManager.h"
 
-//TODO
+#include "Mesh.h"
+
+//******************************************************************
+
+ResourceResult MeshManager::Load( crc32 resource )
+{
+	//TODO
+
+	return RES_FAILED;
+}
+
+ResourceResult MeshManager::Release( crc32 resource )
+{
+	//TODO
+
+	return RES_FAILED;
+}
--- trunk/src/src/libResources/ResourceManager.cpp
+++ trunk/src/src/libResources/ResourceManager.cpp
@@ -1,3 +1,20 @@
 #include "ResourceManager.h"
 
-//TODO
+//******************************************************************
+
+ResourceResult ResourceManager::Load( crc32 resource )
+{
+	return RES_FAILED;
+}
+
+ResourceResult ResourceManager::Release( crc32 resource )
+{
+	return RES_FAILED;
+}
+
+ResourceResult ResourceManager::Clear( void )
+{
+	//TODO
+
+	return RES_SUCCEED;
+}

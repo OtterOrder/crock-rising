82839144aefa3fce16da0aee5eccf614cc049a3b
mesut.yigit41 <mesut.yigit41@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-06 09:43:57 +0000

    Modification uniquement dans libObject.
    Ajout de la class CameraTPS (camera 3ieme personne)
    Code -> celui de UML (lien entre les classes)

--- trunk/src/src/libObjects/Camera.h
+++ trunk/src/src/libObjects/Camera.h
@@ -1,13 +1,17 @@
 #ifndef		_Camera_H
 #define		_Camera_H
 
-//******************************************************************
-
 #include	"Object.h"
 
-//******************************************************************
+class Camera : public Object
+{
+public:
+	Camera(void);
+	Camera( Vector3f pos, Vector3f up, Vector3f target );
 
-//TODO
+private:
+	Vector3f m_Up;
+	Vector3f m_Target;
+};
 
-//******************************************************************
 #endif		// _Camera_H
---
+++ trunk/src/src/libObjects/CameraTPS.h
@@ -0,0 +1,16 @@
+#ifndef		_CameraTPS_H
+#define		_CameraTPS_H
+
+#include	"Camera.h"
+
+class CameraTPS : public Camera
+{
+
+public:
+
+
+private:
+
+};
+
+#endif		// _CameraTPS_H

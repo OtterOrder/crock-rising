2fea906929ad868d6bf30d7ba451f8baf6a174d8
berenger.mantoue <berenger.mantoue@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-06 17:43:05 +0000

    Modification de TextureManager, in progress.

--- src/src/libResources/Texture.cpp
+++ src/src/libResources/Texture.cpp
@@ -1,3 +1,38 @@
 #include "Texture.h"
 
 //TODO
+//******************************************************************
+Texture::Texture()
+{
+	m_pDevice = NULL;
+}
+
+Texture::~Texture()
+{
+}
+
+TextureResult Texture::Load( crc32 resource, TCHAR strTexName)
+{
+	LPDIRECT3DTEXTURE9 pTex;
+
+	pTex = NULL;				//Init du pointeur vers la tex
+
+	if( FAILED(D3DXCreateTextureFromFile(m_pDevice, (LPCSTR)strTexName, &pTex)) )
+		return TEX_FAILED ;
+
+	return TEX_SUCCEED;
+}
+
+TextureResult Texture::Release( crc32 resource )
+{
+	return TEX_FAILED;
+}
+
+TextureResult Texture::SetDevice(LPDIRECT3DDEVICE9 pDevice)
+{ 
+	if (pDevice != NULL)
+		return TEX_FAILED;
+
+	m_pDevice = pDevice;
+	return TEX_SUCCEED;
+}

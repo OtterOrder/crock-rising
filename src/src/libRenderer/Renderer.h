86c0aae3a7feb72387cbfa5090576643caae897b
germain.mazac <germain.mazac@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-08 20:32:56 +0000

    Ajout du renderer de base complet (dxut), System fait désormais appel au renderer (une fenêtre pour l'instant) via la boucle principale.

--- src/src/libRenderer/Renderer.h
+++ src/src/libRenderer/Renderer.h
@@ -3,7 +3,28 @@
 
 //******************************************************************
 
-//TODO
+#include "d3dapp.h"
+#include <libCore/Singleton/Singleton.h>
+
+class Renderer : public Singleton< Renderer >, public CD3DApplication 
+{
+	// Indispensable car Singleton doit pouvoir accéder aux
+	// constructeur/destructeur, qui sont en protected.
+	friend class Singleton< Renderer >;
+
+	public:
+
+		//----------------------------------------------------------
+        // Callback DirectX
+        //----------------------------------------------------------
+
+		HRESULT OnCreateDevice()                           { return S_OK; }
+		HRESULT OnResetDevice()                            { return S_OK; }
+		HRESULT Render()                                   { return S_OK; }
+		HRESULT OnLostDevice()                             { return S_OK; }
+		HRESULT OnDestroyDevice()                          { return S_OK; }
+
+};
 
 //******************************************************************
 #endif		// _Renderer_H

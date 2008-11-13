43bc616a5918d6635d484c34e0805e1c98d8fb66
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-13 17:09:12 +0000

    Ajout des commentaires. Merci de vous en inspirer pour vos codes, ça aidera tout le monde.

--- trunk/src/src/libResources/ResourceManager.cpp
+++ trunk/src/src/libResources/ResourceManager.cpp
@@ -2,26 +2,38 @@
 
 #include "Resource.h"
 
-//******************************************************************
-
+/***********************************************************
+ * Destructeur.
+ **********************************************************/
 ResourceManager::~ResourceManager( void )
 {
 	Clear();
 }
 
-
+/***********************************************************
+ * Charge la ressource.
+ * @param[in]	resource : crc32 de la ressource
+ * @return	le résultat du chargement
+ **********************************************************/
 ResourceResult ResourceManager::Load( crc32 resource )
 {
 	return RES_FAILED;
 }
 
-
+/***********************************************************
+ * Supprime la ressource.
+ * @param[in]	resource : crc32 de la ressource
+ * @return	le résultat de la suppression
+ **********************************************************/
 ResourceResult ResourceManager::Release( crc32 resource )
 {
 	return RES_FAILED;
 }
 
-
+/***********************************************************
+ * Supprime toutes les ressources.
+ * @return	le résultat de la suppression
+ **********************************************************/
 ResourceResult ResourceManager::Clear( void )
 {
 	Resource *pResource = NULL;

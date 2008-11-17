6c87ba5fbc52bf97ab1fb0d79f753cb137e3dfb0
alban.chagnoleau <alban.chagnoleau@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-17 08:16:21 +0000

    Mise à jour du MeshManager et chargement (à finir) des fichiers DAE dans un Mesh

--- src/src/libResources/MeshManager.h
+++ src/src/libResources/MeshManager.h
@@ -13,7 +13,6 @@ class MeshManager : public ResourceManager
 		MeshManager (void)	{};
 		~MeshManager ()		{};
 
-		ResourceResult	Load		( crc32 Resource, LPDIRECT3DDEVICE9 pDevice);		// Charge la ressource
 		ResourceResult	Release		( crc32 Resource );		// Supprime la ressource
 };
 

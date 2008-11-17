d58c82b7448d7470e19d52acdbfb91d738a29d21
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-17 22:46:00 +0000

    WIP: InputManager

--- trunk/src/src/libCore/Inputs/InputManager.cpp
+++ trunk/src/src/libCore/Inputs/InputManager.cpp
@@ -3,6 +3,54 @@
 //******************************************************************
 
 /***********************************************************
+* Vérifie si la touche est appuyée.
+* @param[in]	key	: touche à vérifier
+* @return	true si la touche est appuyée, false sinon
+**********************************************************/
+bool InputManager::IsKeyTriggered( int key )
+{
+	//TODO
+	
+	return false;
+}
+
+/***********************************************************
+* Vérifie si la touche est maintenue appuyée.
+* @param[in]	key	: touche à vérifier
+* @return	true si la touche est maintenue appuyée,
+*			false sinon
+**********************************************************/
+bool InputManager::IsKeyPressed( int key )
+{
+	//TODO
+	
+	return false;
+}
+
+/***********************************************************
+* Vérifie si la touche est relachée.
+* @param[in]	key	: touche à vérifier
+* @return	true si la touche est relachée, false sinon
+**********************************************************/
+bool InputManager::IsKeyReleased( int key )
+{
+	//TODO
+
+	return false;
+}
+
+
+/***********************************************************
+* Donne le vecteur de la souris.
+* @return	vecteur de la souris
+**********************************************************/
+Vector3f InputManager::GetMouseVector( void )
+{
+	return m_MouseVector;
+}
+
+
+/***********************************************************
  * Fonction de rappel des événements.
  * Voir MSDN > WindowProc pour plus d'infos.
  * @param[in]	hWnd	: handle de la fenêtre
@@ -13,7 +61,18 @@
  **********************************************************/
 LRESULT CALLBACK InputManager::EventsCallback( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam )
 {
-	//TODO
+	switch( uMsg )
+	{
+		// Touche du clavier enfoncée
+		case WM_KEYDOWN:
+			//TODO
+			break;
 
+		// Touche du clavier relachée
+		case WM_KEYUP:
+			//TODO
+			break;
+
+	}
 	return S_OK;
 }

026e9ce13adce9c60ae13e69165fcc8851ef3fc4
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-20 17:10:09 +0000

    boulette

--- src/src/System/System.cpp
+++ src/src/System/System.cpp
@@ -68,13 +68,6 @@ int System::MainLoop()
             
         }
 
-		
-		
-		Vector2f lol = InputManager::GetInstance()->GetMouseVector();
-		char pouf[ 128 ];
-		sprintf( pouf, "%f %f\n", lol.x, lol.y );
-		OutputDebugString( pouf );
-
 
     } ; 
 

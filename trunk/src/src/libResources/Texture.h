aa47f7e82bf9ae290d1e684fd468e65b98259707
berenger.mantoue <berenger.mantoue@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-20 10:52:03 +0000

    Parametre de la fonction create texture sont passé en parametre de la fonction initialize et sont mis par défaut.

--- trunk/src/src/libResources/Texture.h
+++ trunk/src/src/libResources/Texture.h
@@ -16,7 +16,19 @@ class Texture: public Resource
 		Texture(void);
 		~Texture();
 
-		ResourceResult			Initialize	(crc32 resource);
+		ResourceResult			Initialize	(crc32			resource,
+											UINT			Width		= D3DX_DEFAULT, 
+											UINT			Height		= D3DX_DEFAULT,
+											UINT			MipLevels	= D3DX_DEFAULT,
+											DWORD			Usage		= 0,
+											D3DFORMAT		Format		= D3DFMT_R8G8B8, 
+											D3DPOOL			Pool		= D3DPOOL_DEFAULT,
+											DWORD			Filter		= D3DX_DEFAULT,
+											DWORD			MipFilter	= D3DX_DEFAULT, 
+											D3DCOLOR		ColorKey	= 0,
+											D3DXIMAGE_INFO	*pSrcInfo	= NULL,
+											PALETTEENTRY 	*pPalette	= NULL);
+
 		ResourceResult			SetDevice	( LPDIRECT3DDEVICE9 pDevice ); //Modifie le device grace au renderer
 		LPDIRECT3DTEXTURE9		GetTex		(){ return m_pTex; }
 

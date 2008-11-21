3bfd619261d3a85a2c49dd8766aa3141999c20ac
mesut.yigit41 <mesut.yigit41@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-21 15:36:23 +0000

    Modification de la camera... en phase de Test. La rotation autour d'un point quelconque ne fonctionne pas.

--- src/src/libObjects/CameraTPS.cpp
+++ src/src/libObjects/CameraTPS.cpp
@@ -1,3 +1,22 @@
 #include "CameraTPS.h"
 
-//TODO
+CameraTPS::CameraTPS( Vector3f pos )
+:Camera(pos)
+{
+
+}
+
+void CameraTPS::RotationAutourPoint(Vector3f point, float angle)
+{
+	D3DXMATRIX t1, rot, t2, tmp;
+	
+	D3DXMatrixRotationY( &rot, angle );						 //Rotation Y a l'origine
+	D3DXMatrixTranslation( &t1, point.x, -25.0f , point.z ); //Translation de O vers point de l'Obj
+
+	//m_Position.x, m_Position.y, m_Position.z -> Offset Camera
+	D3DXMatrixTranslation( &t2, m_Position.x, m_Position.y, m_Position.z ); //Translation Offset Camera 
+
+	D3DXMatrixMultiply( &tmp, &rot, &t1 );
+	D3DXMatrixMultiply( &m_MatrixView, &t2, &tmp );
+
+}

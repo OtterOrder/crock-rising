5987019e79fbc389f653ff03ae30a945af5cf085
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2008-11-30 15:31:57 +0000

    Préfixage des fonctions pour les CRC32 (avec CRC32_). Ajout de fonctions de comparaison de vecteurs.

--- src/src/libCore/Types/Vector.h
+++ src/src/libCore/Types/Vector.h
@@ -1,15 +1,26 @@
 #ifndef		_Vector_H
 #define		_Vector_H
 
-#include <d3dx9.h>
+#include	<d3dx9.h>
 
 //******************************************************************
 
+#define		VEC_PRECISION		0.00001f		// Précision pour les vecteurs
+
+//******************************************************************
 // Vecteur 2f
+
 typedef D3DXVECTOR2 Vector2f, Point2f;
 
+bool VEC_AreAlmostEqual( const Vector2f &v1, const Vector2f &v2 );
+
+
+//******************************************************************
 // Vecteur 3f
+
 typedef D3DXVECTOR3 Vector3f, Point3f;
+
+bool VEC_AreAlmostEqual( const Vector3f &v1, const Vector3f &v2 );
 
 
 //******************************************************************

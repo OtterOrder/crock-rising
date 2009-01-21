498edf814468f1259834786160241e13f07f002a
mesut.yigit41 <mesut.yigit41@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-01-21 10:21:35 +0000

    CameraTPS minimal (pout l'instant Target doit être en pos(0,0,0))
    touche A : reinit position de la cam
    touche Z-S : avance/recule vers le centre (bug possible)
    
    rotation autour du point -> souri

--- trunk/src/src/CrockRising/Levels/LevelStart.cpp
+++ trunk/src/src/CrockRising/Levels/LevelStart.cpp
@@ -27,7 +27,8 @@ LevelStart::~LevelStart( void )
 **********************************************************/
 void LevelStart::Init( void )
 {
-	m_Camera = new Camera();
+	m_Camera = new Camera( Vector3f(0.0f, 10.0f, -100.0f) );
+	m_Camera->SetTarget( Vector3f(10.0f, 10.0f, 0.0f) );
 	Renderer::GetInstance()->SetCamera( m_Camera );	
 }
 
@@ -42,6 +43,48 @@ void LevelStart::Update( void )
 	const int sensibilite = 15;
 	int offsetCursor;
 
+	if( InputManager::GetInstance()->IsKeyPressed( 'Z' ) )
+	{
+		Vector3f pos = m_Camera->GetPosition();
+		Vector3f target	 = m_Camera->GetTarget();
+		Vector3f vectDir = target - pos;
+		float distance = sqrt( vectDir.x*vectDir.x + vectDir.y*vectDir.y + vectDir.z*vectDir.z );
+		float angleY = m_Camera->GetOrientationYRad();
+		
+		//float s = sin( angleY );
+		float c = cos( angleY );
+
+		//pos.x =  pos.x + s;
+		pos.z =  pos.z + c;		
+
+		if( distance>60.0f )
+			m_Camera->SetPosition( pos );
+	}
+
+	if( InputManager::GetInstance()->IsKeyPressed( 'S' ) )
+	{
+		Vector3f pos = m_Camera->GetPosition();
+		Vector3f target	 = m_Camera->GetTarget();
+		Vector3f vectDir = target - pos;
+		float distance = sqrt( vectDir.x*vectDir.x + vectDir.y*vectDir.y + vectDir.z*vectDir.z );
+		float angleY = m_Camera->GetOrientationYRad();
+		
+		//float s = sin( angleY );
+		float c = cos( angleY );
+
+		//pos.x =  pos.x + s;
+		pos.z =  pos.z - c;		
+
+		if( distance<300.0f )
+			m_Camera->SetPosition( pos );
+	}
+
+	if( InputManager::GetInstance()->IsKeyPressed( 'A' ) )
+	{
+		m_Camera->SetPosition(Vector3f(0.0f, 10.0f, -100.0f));
+	}
+
+
 	if( point.x != 0 ) 
 	{
 		offsetCursor = (int)point.x%sensibilite; 
@@ -52,5 +95,6 @@ void LevelStart::Update( void )
 		offsetCursor = (int)point.y%sensibilite; 
 		m_Camera->SetOrientationX( offsetCursor );
 	}
+
 	m_Camera->UpdateMatrixView();
 }

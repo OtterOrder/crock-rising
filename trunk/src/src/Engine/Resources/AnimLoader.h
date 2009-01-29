23c561619f7dc78536f72461f76110bd333b6231
alban.chagnoleau <alban.chagnoleau@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-01-29 17:36:47 +0000

    Commit de Kev: AnimLoader (à terminer)

---
+++ trunk/src/src/Engine/Resources/AnimLoader.h
@@ -0,0 +1,53 @@
+#ifndef _ANIMLOADER_H_
+#define _ANIMLOADER_H_
+
+//******************************************************************
+
+#include	<string>
+#include    <cstring>  //pour la méthode c_str() (p-e include temporaire)
+#include    <vector>
+#include	<TinyXml/tinyxml.h>
+#include	"Core/Types/Vector.h"
+
+#include	"Resource.h"
+
+using namespace std;
+
+//******************************************************************
+
+struct Bone {
+	int iIndice ; 
+	string sName ; 
+	float **fMatrix ; 
+	bool bBoneIsTransform ;
+	vector<Bone> Son ; //Tableau contenant un ou plusieurs fils
+};
+
+
+//******************************************************************
+
+class AnimLoader 
+{
+protected : 
+	int m_iNbBip ; 
+	float ****m_bonesMatrices ;
+	int m_indiceBone ; // indice pour la matrice m_bonesMatrices , variable temporaire (?)
+	string *m_sBonesName ;
+
+public : 
+	AnimLoader ();
+	virtual ~AnimLoader(); 
+
+	ResourceResult Load (const char* sAnimPath ); 
+	ResourceResult FillMatrices ( TiXmlNode *rootNode ); 
+	ResourceResult ExtractMatricesDatas ( TiXmlNode *sourceNode );
+	ResourceResult ConvertTextToArray	( const char* ArrayText, float** &Array, int iCount, int iStride );
+	ResourceResult ExtractHierarchyBones ( TiXmlNode *rootNode );
+
+	Bone			BrowseBonesHierarchy (TiXmlNode* boneNode); //Fonction récursive
+
+};
+
+//******************************************************************
+
+#endif // _ANIMLOADER_H

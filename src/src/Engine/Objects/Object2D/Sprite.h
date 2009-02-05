0c2a2803deca2bf90d52d052707388e296b65b90
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-02-05 17:59:53 +0000

    Mise à jour des Object2D/Sprite : position, scale et alpha fonctionnels. Ajout d'un Math.h qui inclus la lib math standard du c++, tous les types du moteur, et des macros fort utiles.

--- src/src/Engine/Objects/Object2D/Sprite.h
+++ src/src/Engine/Objects/Object2D/Sprite.h
@@ -17,6 +17,15 @@ public:
 
 	virtual void Draw() const;		// Affiche l'objet
 
+	// Transformations d'échelle
+	void SetScale( float scaleX, float scaleY );	// Change l'échelle
+	void SetScale( const Vector2f &scale );			// Change l'échelle
+	Vector2f GetScale() const;						// Donne l'échelle
+
+	// Alpha
+	void SetAlpha( float alpha );	// Change l'alpha (0->1)
+	float GetAlpha() const;			// Donne l'alpha
+
 	// Méthodes spécifiques pour DirectX, appelées lors
 	// de la création/destruction/perte du device Dx.
 	virtual void InitDxData();		// Initialise les données Dx
@@ -25,13 +34,22 @@ public:
 
 protected:
 
+	Vector3f			m_Scale;		// Echelle
+	float				m_Alpha;		// Alpha
+	
 	//crc32				m_TextureID;	// ID de la texture
 	std::string			m_TextureName;	// Nom de la texture
-
+	
 	bool				m_IsDxReady;	// Si les données Dx sont initialisées
-
 	LPD3DXSPRITE		m_pDxSprite;	// Sprite Dx
 	LPDIRECT3DTEXTURE9	m_pDxTexture;	// Texture Dx
+
+private:
+
+	// =========================================================
+	// Méthodes privées
+	
+	void CommonInit();	// Initialisation commune aux constructeurs
 
 };
 

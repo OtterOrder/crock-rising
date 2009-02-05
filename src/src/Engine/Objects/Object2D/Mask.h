e7f96e049845743af5eef530d7cedb36ab8d27f1
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-02-05 22:43:01 +0000

    Oups, vous auriez eut quelques problèmes avec le Engine.vcproj...

---
+++ src/src/Engine/Objects/Object2D/Mask.h
@@ -0,0 +1,25 @@
+#ifndef		_Mask_H
+#define		_Mask_H
+
+//******************************************************************
+
+#include	"Object2D.h"
+
+//******************************************************************
+
+class Mask : public Object2D
+{
+public:
+
+	Mask( float sizeX, float sizeY, const Color4f &color ){}
+	Mask( const Vector2f &size, const Color4f &color ){}
+	virtual ~Mask(){}
+
+protected:
+
+	//TODO
+
+};
+
+//******************************************************************
+#endif		//_Mask_H

b4d5cc5eb9e4f28be602fdba33682ef7392f70d1
berenger.mantoue <berenger.mantoue@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-03-19 20:45:53 +0000

    L'avait pas commit le saligo !

--- src/src/Engine/Objects/SceneObject.cpp
+++ src/src/Engine/Objects/SceneObject.cpp
@@ -4,13 +4,13 @@
 #include	"Resources/ResourceManager.h"
 #include	"Resources/Shader.h"
 #include	"Resources/Mesh.h"
+#include	"../Physics/BoundingBox.h"
 
 using namespace std;
 
 //******************************************************************
 
 list< SceneObject* > SceneObject::RefList;
-//list< BoundingBox* > SceneObject::BoundingBoxList;
 
 //******************************************************************
 
@@ -30,12 +30,14 @@ SceneObject::SceneObject()
 {
 	CommonInit();
 }
-
+//Note : Le parametre position définis l'origine de référence de l'objet qui
+//sera utilisé par le moteur physique. Il convient bien de mettre 0,0,0.
+//à approfondir pour les autres valeurs. La véritable position dans l'espace sera
+//alors donné par la boundingbox.
 SceneObject::SceneObject(const std::string& mesh,
 						 const std::string& Tex,
 						 const D3DXVECTOR3& Position,
-						 const std::string shader,
-						 BoundingBox aBoundingBox)
+						 const std::string shader)
 :Object(Position)
 {	
 	CommonInit();
@@ -44,13 +46,13 @@ SceneObject::SceneObject(const std::string& mesh,
 	m_Tex=Tex;
 	m_Offset=Position;
 
-	BoundingBoxList.push_back(aBoundingBox);
-
 	m_Shader = shader;
+	m_EmpList = new TEmpList;
 }
 
 SceneObject::~SceneObject()
 {
+	m_EmpList->clear(); // suppression dans la liste
 	SceneObject::RefList.remove( this ); // suppression dans la liste
 }
 
@@ -85,10 +87,11 @@ void SceneObject::SetShader(const std::string& Shad)
 void SceneObject::InitObject()
 {
 	m_pDevice = Renderer::GetInstance()->m_pd3dDevice;
-
-	m_PtrMesh=ResourceManager::GetInstance()->Load<Mesh>(m_Mesh);
+		
+	m_PtrMesh = ResourceManager::GetInstance()->Load<Mesh>(m_Mesh);
 	m_MapTexture[TEX_MESH]=ResourceManager::GetInstance()->Load<Texture>(m_Tex);
 	m_PtrShader=ResourceManager::GetInstance()->Load<Shader>(m_Shader.c_str());
+
 
 	//m_PtrShader->m_pEffect->SetTexture("g_MeshTexture", m_MapTexture[TEX_MESH]->m_pTex);
 
@@ -107,35 +110,6 @@ void SceneObject::InitObject()
 	D3DXMatrixMultiply(&m_WorldMatrix, &m_WorldMatrix, &WorldMatrix); 
 }
 
-//////////////////////////////////////////////////////////////////////////
-// Update de l'objet par la moteur physique
-//////////////////////////////////////////////////////////////////////////
-/*
-void SceneObject::Update()
-{
-	BoundingBox *test = &(*BoundingBoxList.begin());
-
-	if( test )
-	{
-		Physicalizer* physXInstance = Physicalizer::GetInstance();
-
-		physXInstance->StartPhysics();
-
-		Draw();
-
-		physXInstance->GetPhysicsResults();
-
-		float DxMat[16];
-		int emp = test->getEmpListe();
-		physXInstance->getScene()->getActors()[ emp ]->getGlobalPose().getColumnMajor44( DxMat );
-
-		D3DXMATRIX matrice( DxMat );
-		m_WorldMatrix = matrice;
-	}
-}
-*/
-
-
 
 
 //===========================================================================//
@@ -150,7 +124,11 @@ void SceneObject::SetTransform(const D3DXMATRIX* world, const D3DXMATRIX* view, 
 {
 	D3DXMATRIX MatWorldView;
 	D3DXMATRIX mWorldViewProjection;
+	D3DXMATRIX Scale;
+
+	D3DXMatrixScaling(&Scale, m_PtrMesh->m_Scale.x, m_PtrMesh->m_Scale.y, m_PtrMesh->m_Scale.z);
 	D3DXMatrixIdentity(&MatWorldView);
+	D3DXMatrixMultiply(&m_WorldMatrix, &m_WorldMatrix, &Scale);
 	D3DXMatrixMultiply(&m_WorldMatrix, &m_WorldMatrix, world);
 	D3DXMatrixMultiply(&MatWorldView, &m_WorldMatrix, view);
 	D3DXMatrixMultiply(&mWorldViewProjection, &MatWorldView, proj);
@@ -198,7 +176,6 @@ void SceneObject::FrameMove(float fElapsedTime)
 
 void SceneObject::Draw()
 {
-
 	m_pDevice->SetVertexDeclaration(m_PtrMesh->m_decl);
 
 	m_PtrShader->m_pEffect->SetTexture("g_MeshTexture", m_MapTexture[TEX_MESH]->m_pTex);
@@ -211,7 +188,6 @@ void SceneObject::Draw()
 
 	if (m_PtrMesh->m_pVB)
 	{
-			
 			m_pDevice->SetStreamSource(0, m_PtrMesh->m_pVB, 0, sizeof(Vertex));
 
 			m_pDevice->SetIndices ( m_PtrMesh->m_pIB );
@@ -244,4 +220,10 @@ void SceneObject::DeleteData()
 	}
 	//m_PtrShader->Release();
 
+}
+
+void SceneObject::SetScale( const Vector3f Scale )
+{
+	if (m_PtrMesh)
+		m_PtrMesh->m_Scale = Scale; 
 }

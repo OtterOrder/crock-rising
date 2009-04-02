9e71513b3ca8961ffb254145604bc1e4400ad686
alban.chagnoleau <alban.chagnoleau@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-04-02 18:08:34 +0000

    Ajout d'un fichier types (et le nom est super !!! Matlab l'adore)

---
+++ src/src/Engine/Core/Types/Types.h
@@ -0,0 +1,13 @@
+#ifndef __TYPES_H
+#define __TYPES_H
+
+typedef		unsigned int		u32;
+typedef		signed int			s32;
+
+typedef		short unsigned int	u16;
+typedef		short signed int	s16;
+
+typedef		unsigned char		u8;
+typedef		signed char			s8;
+
+#endif

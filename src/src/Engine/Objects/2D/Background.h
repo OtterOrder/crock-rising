a5dddacaaf44cddb5ec79ec772375bac2f110f3e
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-04-02 10:21:20 +0000

    Ajout de la classe background pour avoir des fonds 2D.

---
+++ src/src/Engine/Objects/2D/Background.h
@@ -0,0 +1,26 @@
+#ifndef		_Background_H_
+#define		_Background_H_
+
+//******************************************************************
+
+#include	"Sprite.h"
+
+//******************************************************************
+
+class Background : public Sprite
+{
+public:
+
+	//-- Méthodes publiques
+	
+	Background( const char *path );
+	virtual ~Background();
+
+protected:
+
+	//TODO
+
+};
+
+//******************************************************************
+#endif		//_Background_H_

d3ab24fbbb92aa3fa72ebeb6afbc738e9b4c5fc0
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-04-02 10:08:12 +0000

    Ajout d'un système de sauvegarde.

--- trunk/src/src/Engine/Game/Game.h
+++ trunk/src/src/Engine/Game/Game.h
@@ -21,33 +21,45 @@ class Game : public Singleton< Game >
 
 public:
 
-	// =========================================================
-	// Méthodes publiques
+	//-- Structures publiques
+
+	struct Backup
+	{
+		// A dériver..
+		// La sauvegarde sert à ce qu'on veut. On peut y stocker toutes les
+		// données globales du jeu. Il serait également judicieux d'y ajouter
+		// une interface de chargement/sauvegarde dans un fichier, etc.
+
+		virtual ~Backup(){}
+	};
+
+	//-- Méthodes publiques
 
 	void	Update			( void );			// Update du jeu
+
+	void	StoreBackup		( Backup *pBackup );
+	Backup*	GetBackup		( void ) const;
 	
 	void	ChangeLevel		( crc32 levelID );	// Change de niveau
 	crc32	GetLevelID		( void ) const;		// Donne l'ID du niveau courant
 	crc32	GetPrevLevelID	( void ) const;		// Donne l'ID du niveau précédent
 
-
 protected:
 
-	// =========================================================
-	// Types protégés
+	//-- Types protégés
 
 	typedef std::list< Object* >::iterator		ObjIt;		// Itérateur d'objet
 
-	// =========================================================
-	// Données protégées
+	//-- Données protégées
+	
+	Backup					*m_Backup;			// Sauvegarde du jeu (ce qu'on veut)
 	
 	Level					*m_CurrentLevel;	// Niveau courant
 	crc32					m_PrevLevelID;		// ID du niveau précédent
 
 	std::list< Object* >	*m_ObjList;			// Liste des objets 3d
 
-	// =========================================================
-	// Méthodes protégées
+	//-- Méthodes protégées
 
 	Game			( void );		// Constructeur
 	virtual ~Game	( void );		// Destructeur

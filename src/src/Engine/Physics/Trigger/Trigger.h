367d449f376436e5a52abb9d34e759783292f8fc
berenger.mantoue <berenger.mantoue@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-04-09 17:55:35 +0000

    Nettoyage et commentaires

--- src/src/Engine/Physics/Trigger/Trigger.h
+++ src/src/Engine/Physics/Trigger/Trigger.h
@@ -1,16 +1,16 @@
-// ===============================================================================
-//						     PHYSX SDK TRAINING PROGRAMS
-//			                       TRIGGER REPORT
-//
-//						   Written by Bob Schade, 12-15-05
-// ===============================================================================
-
 #ifndef TRIGGER_H
 #define TRIGGER_H
 
 #include "UserData.h"
+#include "../Physicalizer.h"
 
-
+/////////////////////////////////////////////////////////////////////////////////
+// Classe de gestion des triggers. Elle est fournis à l'instance de physX dans //
+// de Physicalizer par "m_Scene->setUserTriggerReport(&gTriggerReport);"	   //
+// PhysX l'appelera dès qu'une collision aura lieux avec les actors possedant  //
+// le flag NX_TRIGGER_ENABLE. Elle lancera les fonctions donnée à l'acteur lors//
+// de sa création dans son userdata.										   //
+/////////////////////////////////////////////////////////////////////////////////
 class TriggerReport : public NxUserTriggerReport
 {
 public:
@@ -18,41 +18,34 @@ public:
 	//Fonction de gestions des évenements lors des contacts avec les triggers
 	virtual void onTrigger(NxShape& triggerShape, NxShape& otherShape, NxTriggerFlag status)
 	{
-		NxU32 i = 0;
-
-		NxActor** actors = scene->getActors();
-		NxU32 nbActors = scene->getNbActors();
-		while (nbActors--)
+		if(scene)
 		{
-			NxActor* actor = actors[nbActors];
-			ActorUserData *ud = (ActorUserData*)(actor->userData);
-			if(ud)
+			NxActor** actors = scene->getActors();
+			NxU32 nbActors = scene->getNbActors();
+			while (nbActors--)
 			{
-				switch( status )
+				actors = scene->getActors(); //Si les fonctions ci dessous créeent des acteurs
+											 //la liste actors n'est plus valide. Donc, il faut la recharger.
+				NxActor* actor = actors[nbActors];
+				ActorUserData *ud = (ActorUserData*)(actor->userData);
+				if(ud)
 				{
-					case NX_TRIGGER_ON_ENTER : if(ud->OnEnterFunc) ud->OnEnterFunc();
-						break;
-					case NX_TRIGGER_ON_LEAVE : if(ud->OnLeaveFunc) ud->OnLeaveFunc();
-						break;
-					case NX_TRIGGER_ON_STAY :  if(ud->OnStayFunc) ud->OnStayFunc();
-						break;
-					default: break;
+					switch( status )
+					{
+						case NX_TRIGGER_ON_ENTER : if(ud->OnEnterFunc) ud->OnEnterFunc();
+							break;
+						case NX_TRIGGER_ON_LEAVE : if(ud->OnLeaveFunc) ud->OnLeaveFunc();
+							break;
+						case NX_TRIGGER_ON_STAY :  if(ud->OnStayFunc) ud->OnStayFunc();
+							break;
+						default: break;
+					}
 				}
 			}
-
-			// Mark actors in the trigger area to apply forcefield forces to them
-			NxActor* triggerActor = &triggerShape.getActor();
-			/*if (((ActorUserData*)(triggerActor->userData))->flags & UD_IS_TRIGGER)
-			{
-				if (status & NX_TRIGGER_ON_STAY)
-				{
-					NxActor* otherActor = &otherShape.getActor();
-					((ActorUserData*)(otherActor->userData))->flags |= UD_IS_INSIDE_TRIGGER;
-				}
-			}*/
 		}
 	}
 } gTriggerReport;
+
 
 #endif  // TRIGGER_REPORT_H
 

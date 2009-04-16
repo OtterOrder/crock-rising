ca993cac82de2ef741fdeca7d1a13b84777a0be9
faure.kevin42 <faure.kevin42@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-04-16 17:48:19 +0000

    Suppression de la donnée membre m_position

--- trunk/src/src/CrockRising/Characters/Enemy.cpp
+++ trunk/src/src/CrockRising/Characters/Enemy.cpp
@@ -4,7 +4,7 @@ Enemy::Enemy()
 {
 	m_iLife = 100 ;
 
-	m_pAnimated = new SceneObjectAnimated("Alien_Mesh.DAE","Alien_Anim.DAE",m_position);
+	m_pAnimated = new SceneObjectAnimated("Alien_Mesh.DAE","Alien_Anim.DAE",D3DXVECTOR3(0.f,-17.f,0.f));
 	m_pAnimated->InitObject();
 	m_pAnimated->InitDeviceData();
 }

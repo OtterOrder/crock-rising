756a52c7d94920a67e54cbb1958ed3d299657c5e
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-05-27 20:49:03 +0000

    Correction de 2 warnings dans le meshloader qui font chier depuis longtemps -_-. Correction du Init dans le projet CrockRising pour que ça compile (suite à la modif du moteur).

--- trunk/src/src/CrockRising/Characters/Enemy.cpp
+++ trunk/src/src/CrockRising/Characters/Enemy.cpp
@@ -5,8 +5,7 @@ Enemy::Enemy()
 	m_iLife = 100 ;
 
 	m_pAnimated = new SceneObjectAnimated("Alien_Mesh.DAE","Alien_Anim.DAE",D3DXVECTOR3(0.f,-17.f,0.f));
-	m_pAnimated->InitObject();
-	m_pAnimated->InitDeviceData();
+	m_pAnimated->Init();
 }
 
 Enemy::~Enemy()

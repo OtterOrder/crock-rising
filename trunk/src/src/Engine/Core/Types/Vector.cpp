18c5f6a48490a36322727ce5854686d5fa05fe10
mathieu.chabanon <mathieu.chabanon@7c6770cc-a6a4-11dd-91bf-632da8b6e10b>
2009-05-28 20:17:56 +0000

    Remplacement de la structure Coord (qui n'était jamais utilisée d'ailleurs o_O) par Vector2i.

--- trunk/src/src/Engine/Core/Types/Vector.cpp
+++ trunk/src/src/Engine/Core/Types/Vector.cpp
@@ -43,3 +43,24 @@ bool VEC_AreAlmostEqual( const Vector4f &v1, const Vector4f &v2 )
 		&& ( fabs( v1.z - v2.z ) < VEC_PRECISION )
 		&& ( fabs( v1.w - v2.w ) < VEC_PRECISION );
 }
+
+//**********************************************************
+// Constructeur de Vector2i avec paramètres.
+// @param[in]	_x	: coordonnée x
+// @param[in]	_y	: coordonnée y
+//**********************************************************
+Vector2i::Vector2i( s32 _x, s32 _y )
+{
+	x = _x;
+	y = _y;
+}
+
+//**********************************************************
+// Constructeur de Vector2i par copie.
+// @param[in]	_v	: vecteur
+//**********************************************************
+Vector2i::Vector2i( const Vector2i &_v )
+{
+	x = _v.x;
+	y = _v.y;
+}
